2d4aed3d7b5b70c984b634f67d3056d314a15494
Ashley Solomon <ashley@lograph.co.jp>
2021-03-27 15:51:44 +0800

    wip here #8

---
+++ components/organisms/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center font-sans-bold mx-auto text-center">
+        <h1 class="font-extrabold text-center text-8xl">
+            <span class="block"> We build better </span>
+            <span class="block"> Web Experience </span>
+            <span class="block"> that will outsoar your company </span>
+        </h1>
+    </div>
+</template>
+<script>
+export default {};
+</script>
