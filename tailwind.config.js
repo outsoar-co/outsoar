23d6d4c8e0c72144ffcee21cb416219be6f73cae
Ashley Solomon <ashcslmn@gmail.com>
2021-04-21 12:45:09 +0800

    fix container

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@
 module.exports = {
     darkMode: 'class',
     theme: {
+        container: {
+            screens: {
+                sm: '100%',
+                md: '100%',
+                lg: '1024px',
+                xl: '1280px',
+            },
+        },
         fontFamily: {
             sans: ['TitilliumWeb-Regular', 'sans-serif'],
             'sans-light': ['TitilliumWeb-Light', 'sans-serif'],
