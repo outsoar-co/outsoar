1b28563faf68ad30c4c25a2b82d0115c964712ee
macangel2303 <macangel2303@gmail.com>
2022-09-06 15:32:56 +0800

    developmen and design contents

--- content/services/development-and-design.md
+++ content/services/development-and-design.md
@@ -3,6 +3,64 @@ title: Development and Design
 description: Development and Design
 ---
 
-# Welcome
 
-From fully understanding your unique business so that we can deliver the ideal solution, to implementing that solution to bring you closer to your customers -- everything we do is centered around human connection. In addition to having worked with multiple Fortune 500 companies, we are passionate about staying up-to-date on the latest technologies and we love discovering creative ways to utilize them for our clients. Whether it's helping to automate workflow or creating a digital strategy to boost your online success, we strive to be one thing: your trusted digital partner.
+<segment>
+    <strong class="underline text-md">We build better</strong>. 
+    <strong>Outsoar PH</strong> provides the services we can offer at the best by meeting beyond the client's expectations. <strong>Outsoar</strong> main goal - Turning bright ideas into amazing and scalable digital deliverables. Our team's web designers, graphic designers, and developers have the highest ability to perform excellence in every project. The Ability to provide the best websites is on us.
+</segment>
+
+### Staff Augmentation
+
+<segment>
+    <ul>
+        <li class="uppercase">Skill matching</li>
+    </ul>
+</segment>
+
+<segment>
+    <strong>Outsoar PH </strong> will assist you in determining the demands of your project to find the best 
+    solution for your firm. You maintain direct touch with project managers or lead 
+    developers and maintain control over your project. Highly skilled and qualified 
+    developers that provide in the gap.
+</segment>
+
+
+### Full-stack, Backend, and Frontend Development
+
+
+<segment>
+    <ul class="uppercase">
+        <li>Node JavaScript/Typescript Developers</li>
+        <li>Php Developers (Laravel)</li>
+        <li>React Native Developers</li>
+        <li>React Js Developers</li>
+        <li>Vue Js Developers</li>
+    </ul>
+</segment>
+
+Our full-stack front-end and back-end developers are professionals in their fields.
+Server-side applications such as apps and APIs.
+We concentrate on leveraging technology that not only makes your solutions more 
+appealing but also improves their performance. We ensure that the UI/UX of your 
+websites, apps, and goods is user-friendly.
+
+
+
+
+### WordPress Development
+
+<segment>
+    <ul class="uppercase">
+        <li>Custom Developer</li>
+        <li>No code development</li>
+    </ul>
+</segment>
+
+We are in charge of both front-end and back-end development, as well as theme
+and plugin installation. Our objective is to design appealing and user-friendly websites
+based on customer requirements.
+
+We have an extensive understanding of front-end programming languages, a keen 
+aesthetic sense, and excellent content management abilities. Finally, a top-tier 
+WordPress developer who can produce appealing, user-friendly websites that precisely 
+suit your design and functionality requirements.
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
     <main class="bg-light-vue dark:bg-dark-vue">
         <hero />
-        <segment class="bg-grayish-red-200 dark:bg-dark-odd shadow-inner">
+        <segment class="shadow-inner bg-grayish-red-200 dark:bg-dark-odd">
             <card
                 class="container px-6 py-20 transform -translate-y-12 lg:transform lg:-translate-y-32"
             >
                 <template #body>
-                    <span class="flex text-base justify-center text-center"
+                    <span class="flex justify-center text-base text-center"
                         >Our services</span
                     >
-                    <h-title size="text-3xl" class="text-center mb-4">
+                    <h-title size="text-3xl" class="mb-4 text-center">
                         We are experts in
                     </h-title>
                     <list
@@ -21,13 +21,13 @@
                             },
                             item: {
                                 basedClass:
-                                    'flex flex-col w-full lg:w-1/3 gap-6 lg:p-8 p-4',
+                                    'flex flex-col w-full lg:w-1/3 gap-6 lg:p-8 p-4 text-center',
                             },
                         }"
                     >
                         <template #icon="{ item }">
                             <div
-                                class="bg-grayish-red-200 dark:bg-dark-even rounded-sm shadow-sm"
+                                class="rounded-sm shadow-sm bg-grayish-red-200 dark:bg-dark-even"
                             >
                                 <component
                                     :is="item.props.banner.icon"
@@ -39,7 +39,7 @@
                             </div>
                         </template>
                         <template #to="{ item }">
-                            <div class="flex-none flex py-4 justify-center">
+                            <div class="flex justify-center flex-none py-4">
                                 <btn-link :to="item.slug">Learn more</btn-link>
                             </div>
                         </template>
@@ -56,9 +56,9 @@
                     (index + 2) % 2 !== 0,
             }"
         >
-            <div class="flex flex-col justify-center items-center">
+            <div class="flex flex-col items-center justify-center">
                 <div
-                    class="flex flex-wrap justify-center lg:flex-row lg:flex-nowrap lg:gap-x-8 my-24"
+                    class="flex flex-wrap justify-center my-24 lg:flex-row lg:flex-nowrap lg:gap-x-8"
                     :class="{
                         'flex-wrap-reverse lg:flex-row-reverse':
                             (index + 2) % 2 !== 0,
@@ -74,7 +74,7 @@
                             "
                         >
                             <div
-                                class="rounded-sm p-8 mx-auto"
+                                class="p-8 mx-auto rounded-sm"
                                 :class="{
                                     'bg-grayish-red-200 dark:bg-dark-odd':
                                         (index + 2) % 2 == 0,
@@ -84,7 +84,7 @@
                             >
                                 <component
                                     :is="section.props.banner.icon"
-                                    class="fill-current object-contain h-64 lg:h-96 w-auto mx-auto p-4"
+                                    class="object-contain w-auto h-64 p-4 mx-auto fill-current lg:h-96"
                                 />
                             </div>
                         </template>
@@ -98,7 +98,7 @@
                             <component :is="section.props.banner.icon" />
                         </template>
                     </div>
-                    <p-article class="w-5/6 lg:w-1/2 p-8 lg:p-4">
+                    <p-article class="w-5/6 p-8 lg:w-1/2 lg:p-4">
                         <span class="text-base">{{ section.title }}</span>
                         <h1>{{ section.props.banner.text }}</h1>
                         <p>{{ section.body }}</p>
@@ -109,7 +109,7 @@
                             <div class="flex flex-row items-center">
                                 <span>{{ section.props.linkText }}</span>
                                 <chevron-double-right
-                                    class="fill-current ml-1"
+                                    class="ml-1 fill-current"
                                 />
                             </div>
                         </nuxt-link>
